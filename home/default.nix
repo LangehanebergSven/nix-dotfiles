528d4730625d76926b26a448886fe11ef320ebb1
Sven Langehaneberg <s.langehaneberg@shopware.com>
2025-05-22 17:21:09 +0200

    fix syntax

--- home/default.nix
+++ home/default.nix
@@ -3,8 +3,6 @@
 , ...
 }: {
 
-  imports = [
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
